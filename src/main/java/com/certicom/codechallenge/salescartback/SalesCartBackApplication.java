99b3dd0a9fb900d3c61e2e9e8e9325941f3205b3
Sebastian Alfaro <sebas43243@hotmail.com>
2022-10-06 17:46:34 -0500

    initial commit

---
+++ src/main/java/com/certicom/codechallenge/salescartback/SalesCartBackApplication.java
@@ -0,0 +1,13 @@
+package com.certicom.codechallenge.salescartback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesCartBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesCartBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/certicom/codechallenge/salescartback/SalesCartBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.certicom.codechallenge.salescartback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesCartBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
